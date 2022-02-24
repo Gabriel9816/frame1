c0de09322d08e59cf898ef49f9becbc02e2dd97d
iTOSHIBA <99374967+Gabriel9816@users.noreply.github.com>
2022-02-23 22:45:13 -0400

    final aula

--- src/main/java/br/edu/ifms/frame1/repository/UserRepository.java
+++ src/main/java/br/edu/ifms/frame1/repository/UserRepository.java
@@ -1,28 +1,15 @@
 package br.edu.ifms.frame1.repository;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.edu.ifms.frame1.model.User;
 
 @Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User, UUID>{
     
-    private static final Logger logger = LoggerFactory.getLogger(UserRepository.class);
-
-    //Selecto * from tb_user
-    public List<User> findAll(){
-        var users = new ArrayList<User>();
-        users.add(new User(UUID.randomUUID(), "Gabriel", "gabriel@email.com"));
-        users.add(new User(UUID.randomUUID(), "Caio", "caio@email.com"));
-        users.add(new User(UUID.randomUUID(), "Eduardo Pavanelli", "eduardo.pavanelli@email.com"));
-        users.add(new User(UUID.randomUUID(), "Stella", "stella@email.com"));
-        logger.info("Listando os Arquivos.....");
-        return users;
-    }
+ 
+   
 }
